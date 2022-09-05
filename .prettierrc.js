e5ac4d55a36fdddb79d93a92c8448b01cefe3fb7
Chris Fields II <chrisjr06@gmail.com>
2022-09-05 12:01:29 -0500

    feat: createView utility

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: true,
+};
---
+++ src/createView.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export type DefaultModel = Record<string, unknown>;
+
+export type ViewModelProps<Model extends DefaultModel = DefaultModel> =
+  | {
+      status: 'success';
+      model: Model;
+    }
+  | { status: 'loading' }
+  | { status: 'error'; error: Error }
+  | { status: 'empty' };
+
+export type FailureProps = {
+  error: Error;
+};
+
+export type CreateViewInput<Model extends DefaultModel> = {
+  Success: (props: Model) => JSX.Element;
+  Loading?: () => JSX.Element;
+  Failure?: (props: FailureProps) => JSX.Element;
+  Empty?: () => JSX.Element;
+};
+
+export const RenderNull = () => null;
+
+/**
+ * Create a UI `View` given a `ViewModel` and components that represent different
+ * states like `Success`, `Loading`, `Empty`, `Failure`.
+ */
+export const createView = <Model extends DefaultModel>(
+  input: CreateViewInput<Model>
+): React.FC<ViewModelProps<Model>> => {
+  const {
+    Success,
+    Failure = RenderNull,
+    Loading = RenderNull,
+    Empty = RenderNull,
+  } = input;
+
+  return (props) => {
+    const { status } = props;
+
+    if (status === 'error') {
+      return <Failure {...props} />;
+    }
+
+    if (status === 'loading') {
+      return <Loading />;
+    }
+
+    if (status === 'empty') {
+      return <Empty />;
+    }
+
+    if (status === 'success') {
+      const { model } = props;
+      return <Success {...model} />;
+    }
+
+    return null;
+  };
+};
