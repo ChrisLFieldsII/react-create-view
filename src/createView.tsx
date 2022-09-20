966dd488f24c5744b98da1f673812c130de76cc5
Chris Fields II <chrisjr06@gmail.com>
2022-09-20 00:11:43 -0500

    createView accepts generic types for each state

--- src/createView.tsx
+++ src/createView.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 
 export type DefaultModel = Record<string, unknown>;
 
-export type ViewModelProps<Model extends DefaultModel = DefaultModel> =
+export type ViewModelProps<
+  SuccessModel extends DefaultModel = DefaultModel,
+  FailureModel extends DefaultModel = DefaultModel,
+  LoadingModel extends DefaultModel = DefaultModel,
+  EmptyModel extends DefaultModel = DefaultModel
+> =
   | {
       status: 'success';
-      model: Model;
+      model: SuccessModel;
     }
-  | { status: 'loading' }
-  | { status: 'error'; error: Error; onRetry?: () => void }
-  | { status: 'empty' };
+  | { status: 'loading'; model?: LoadingModel }
+  | { status: 'error'; model?: FailureModel }
+  | { status: 'empty'; model?: EmptyModel };
 
-export type FailureProps = {
-  error: Error;
-  onRetry?: () => void;
-};
-
-export type CreateViewInput<Model extends DefaultModel> = {
-  Success: (props: Model) => JSX.Element;
-  Loading?: () => JSX.Element;
-  Failure?: (props: FailureProps) => JSX.Element;
-  Empty?: () => JSX.Element;
+export type CreateViewInput<
+  SuccessModel extends DefaultModel = DefaultModel,
+  FailureModel extends DefaultModel = DefaultModel,
+  LoadingModel extends DefaultModel = DefaultModel,
+  EmptyModel extends DefaultModel = DefaultModel
+> = {
+  Success: (props: SuccessModel) => JSX.Element;
+  Loading?: (props: LoadingModel) => JSX.Element;
+  Failure?: (props: FailureModel) => JSX.Element;
+  Empty?: (props: EmptyModel) => JSX.Element;
 };
 
 export const RenderNull = () => null;
@@ -29,9 +34,16 @@ export const RenderNull = () => null;
  * Create a UI `View` given a `ViewModel` and components that represent different
  * states like `Success`, `Loading`, `Empty`, `Failure`.
  */
-export const createView = <Model extends DefaultModel>(
-  input: CreateViewInput<Model>
-): React.FC<ViewModelProps<Model>> => {
+export const createView = <
+  SuccessModel extends DefaultModel = DefaultModel,
+  FailureModel extends DefaultModel = DefaultModel,
+  LoadingModel extends DefaultModel = DefaultModel,
+  EmptyModel extends DefaultModel = DefaultModel
+>(
+  input: CreateViewInput<SuccessModel, FailureModel, LoadingModel, EmptyModel>
+): React.FC<
+  ViewModelProps<SuccessModel, FailureModel, LoadingModel, EmptyModel>
+> => {
   const {
     Success,
     Failure = RenderNull,
@@ -41,21 +53,22 @@ export const createView = <Model extends DefaultModel>(
 
   return (props) => {
     const { status } = props;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const model: any = props?.model || {};
 
     if (status === 'error') {
-      return <Failure {...props} />;
+      return <Failure {...model} />;
     }
 
     if (status === 'loading') {
-      return <Loading />;
+      return <Loading {...model} />;
     }
 
     if (status === 'empty') {
-      return <Empty />;
+      return <Empty {...model} />;
     }
 
     if (status === 'success') {
-      const { model } = props;
       return <Success {...model} />;
     }
 
